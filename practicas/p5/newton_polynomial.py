2c4185eb0b97ab71a933bc390fe2fb8e30e0a826
Pablo Cabeza <lemniscata.lmn@gmail.com>
2015-04-09 00:59:01 +0200

    minor style changes

--- practicas/p5/newton_polynomial.py
+++ practicas/p5/newton_polynomial.py
@@ -19,7 +19,7 @@ def divdiff(X,Y):
     res = np.empty(len(X))
     res[0] = Y[0]
 
-    for i in range(1,len(X)):        
+    for i in xrange(1,len(X)):        
         Y = (Y[1:] - Y[:-1]) / (X[i:] - X[:-i])
         res[i] = Y[0]
     return res
@@ -49,13 +49,14 @@ def newton_polynomial(y, tau, num_points=100, libraries=False):
     if libraries == False:
         dd = divdiff(tau,y)
         res = np.empty((len(tau), num_points))
+
         res[0].fill(1) # i = 0 -> base case
 
         tau_a = np.subtract(t, tau[:-1][None].T, res[1:]) # tau_a[i] = res[i+1] = t - tau[i] 
         np.multiply.accumulate(tau_a, out = tau_a) # tau_a[i] = mult[j=0,i](t - tau[j])
-        np.multiply(res, dd[None].T, res) # res[i] = tau_a[i]*dd[i] = prod i factor of the newton polynomial
+        np.multiply(res, dd[None].T, res) # res[i] = tau_a[i]*dd[i] = factor i of the newton polynomial
 
-        return res.sum(0)
+        return res.sum(0) # return sum of factors
 
     else:
         return np.polyval(np.polyfit(tau,y,len(tau)-1), t)
