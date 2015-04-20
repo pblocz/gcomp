691094ff54efa1ffad7644a154998e7635e39a2d
Pablo Cabeza <lemniscata.lmn@gmail.com>
2015-04-20 19:24:02 +0200

    many changes cosmetic changes

--- practicas/p5/polynomial_curve_fitting.py
+++ practicas/p5/polynomial_curve_fitting.py
@@ -15,7 +15,7 @@ import logging
 # logger = logging.
 
 
-def polynomial_curve_fitting(points, knots, method, *args, **kwargs): # L=0, libraries=False, num_points=100):    
+def polynomial_curve_fitting(points, knots, method, num_points=100, libraries=False, L=0, degree=None): # L=0, libraries=False, num_points=100):    
     '''
        Fits planar curve to points at given knots. 
 
@@ -41,25 +41,26 @@ def polynomial_curve_fitting(points, knots, method, *args, **kwargs): # L=0, lib
            numpy array of shape (num_points, 2) given by the evaluation of the polynomial
            at the evenly spaced num_points between tau[0] and tau[-1]
     '''
-    if method == "newton": return newton_polynomial(points, knots, *args, **kwargs)
+
+    if knots == 'chebyshev': knots = chebyshev_knots(0,1,len(points))
+
+    if method == "newton": return newton_polynomial(points, knots, num_points, libraries)
     elif method == "least_squares": 
-      return least_squares_fitting(points,knots, kwargs.pop("degree",None) or len(knots) - 1, 
-                                   *args, **kwargs)
-    
-def divdiff(X,Y):
-    '''
-    Compute the divided difference of the pairs zip(X,Y)
+      return least_squares_fitting(points, knots, degree or (len(knots) - 1), num_points,  L, libraries)
 
-    returns: a ndarray where r[i] = [y_0...y_i]
-    '''
-    res = np.empty(len(X))
-    res[0] = Y[0]
+def divdifnd(y,tau):
+  "Compute divided differences for y of dimension (n,m), m > 1"
 
+  dd = np.empty(y.shape); dd[0] = y[0]
+  X = np.tile(tau,(y.shape[1],1,)).T
+  yaux = y
 
-    for i in xrange(1,len(X)):        
-        Y = (Y[1:] - Y[:-1]) / (X[i:] - X[:-i])
-        res[i] = Y[0]
-    return res
+  for i in xrange(1,len(tau)):        
+      yaux = (yaux[1:] - yaux[:-1]) / (X[i:] - X[:-i])
+      dd[i] = yaux[0]
+
+  return dd
+
 
 def newton_polynomialnd(y,tau, num_points, out = None):
     '''
@@ -68,7 +69,7 @@ def newton_polynomialnd(y,tau, num_points, out = None):
     subtracttions, which translates into 5 numpy calls with no python 
     for-loops.
 
-    It runs in (except for divdiff()):
+    It runs in (except for divdifnd()):
 
         O(n*p) operations inside numpy
         O(1) numpy calls
@@ -78,71 +79,20 @@ def newton_polynomialnd(y,tau, num_points, out = None):
     t = np.linspace(tau[0], tau[-1], num_points)
     if out is None: out = np.empty((num_points,y.shape[1],))
 
+    dd = divdifnd(y,tau)
 
-    # Compute divided diferences
-
-    dd = np.empty(y.shape); dd[0] = y[0]
-    yaux = y; X = np.tile(tau,(2,1,)).T
-
-    for i in xrange(1,len(tau)):        
-        yaux = (yaux[1:] - yaux[:-1]) / (X[i:] - X[:-i])
-        dd[i] = yaux[0]
-
-
-    # Compute ...
-
+    res = np.empty((len(tau), num_points)); res[0].fill(1) # i = 0 -> base case
+    tau_a = np.subtract(t, tau[:-1][None].T, res[1:]) # tau_a[i] = res[i+1] = t - tau[i] 
+    np.multiply.accumulate(tau_a, out = tau_a) # tau_a[i] = mult[j=0,i](t - tau[j])
+    
+    aux = np.empty_like(res)
     for i in xrange(dd.shape[1]):
-      res = np.empty((len(tau), num_points))
-
-      res[0].fill(1) # i = 0 -> base case
-
-      tau_a = np.subtract(t, tau[:-1][None].T, res[1:]) # tau_a[i] = res[i+1] = t - tau[i] 
-      np.multiply.accumulate(tau_a, out = tau_a) # tau_a[i] = mult[j=0,i](t - tau[j])
-      np.multiply(res, dd[:,i][None].T, res) # res[i] = tau_a[i]*dd[i] = factor i of the newton polynomial
-
-      res.sum(0, out = out[:,i]) # sum of factors
-
-    # res = np.empty((len(tau), num_points,dd.shape[1]))
-    # res[0].fill(1) # i = 0 -> base case
-
-    # tau_a = np.subtract(t, tau[:-1][None].T, res[1:]) # tau_a[i] = res[i+1] = t - tau[i] 
-    # np.multiply.accumulate(tau_a, out = tau_a) # tau_a[i] = mult[j=0,i](t - tau[j])
-    # np.multiply(res, dd[:,i][None].T, res) # res[i] = tau_a[i]*dd[i] = factor i of the newton polynomial
-
-    # res.sum(0, out = out[:,i]) # sum of factors
-
+      np.multiply(res, dd[:,i][None].T, out = aux) # res[i] = tau_a[i]*dd[i] = factor i of the newton polynomial
+      aux.sum(0, out = out[:,i]) # sum of factors
 
     return out
 
-def newton_polynomial1d(y,tau, num_points, out = None):
-    '''
-    It uses naïve polynomial evalutation instead of horner algorithm. For
-    each t it does 2*n-1 multiplications, n-1 additions and n-1
-    subtracttions, which translates into 5 numpy calls with no python 
-    for-loops.
-
-    It runs in (except for divdiff()):
-
-        O(n*p) operations inside numpy
-        O(1) numpy calls
-        O(n*p) space storage 
-    '''
-
-    t = np.linspace(tau[0], tau[-1], num_points)
-    if out is None: out = np.empty(num_points)
-
-    dd = divdiff(tau,y)
-    res = np.empty((len(tau), num_points))
-
-    res[0].fill(1) # i = 0 -> base case
-
-    tau_a = np.subtract(t, tau[:-1][None].T, res[1:]) # tau_a[i] = res[i+1] = t - tau[i] 
-    np.multiply.accumulate(tau_a, out = tau_a) # tau_a[i] = mult[j=0,i](t - tau[j])
-    np.multiply(res, dd[None].T, res) # res[i] = tau_a[i]*dd[i] = factor i of the newton polynomial
-
-    return res.sum(0, out = out) # sum of factors
-
-def newton_polynomial(y, tau, num_points=100, libraries=False, *args, **kwargs):
+def newton_polynomial(y, tau, num_points=100, libraries=False):
     '''
     Computes de Newton's polynomial interpolating values x at knots tau
     y: numpy array of size n; points to interpolate
@@ -156,61 +106,59 @@ def newton_polynomial(y, tau, num_points=100, libraries=False, *args, **kwargs):
     returns:
        numpy array of size num_points given by the polynomial 
        evaluated at np.linspace(tau[0], tau[1], num_points)
-
-    Maximum cost allowed: 5,43 s at lab III computers
-            degree = n - 1 = 9
-            num_points = 100
     '''
 
-    t = np.linspace(tau[0], tau[-1], num_points)
-
-    if libraries == False:
-        # r_val = np.empty((2, num_points))
-        # newton_polynomial1d(y[:,0],tau, num_points, out = r_val[0])
-        # newton_polynomial1d(y[:,1],tau, num_points, out = r_val[1])
-        # return r_val.T
-
-        return newton_polynomialnd(y, tau, num_points)
-        
+    if libraries == False: return newton_polynomialnd(y, tau, num_points)    
     else: 
-        fit = np.polyfit(tau,y,len(tau)-1)
-        return np.vstack([np.polyval(fit[:,i],t) for i in [0,1]]).T
+      t = np.linspace(tau[0], tau[-1], num_points)
+      fit = np.polyfit(tau,y,len(tau)-1)
+      return np.vstack([np.polyval(fit[:,i],t) for i in xrange(fit.shape[1])]).T
 
 
-def least_squares_fitting(points, knots, degree, num_points, L=0, libraries=True, *args, **kwargs):    
-    #I've used np.linalg.lstsq and np.polyval if libraries==True
+def least_squares_fitting(points, knots, degree, num_points, L=0, libraries=True):    
     t = np.linspace(knots[0], knots[-1], num_points)
 
     if libraries == False:
-      # Compute vardemond matrix transposed (fast)
+      # Compute vardemond matrix transposed `C` (fast) (in the algo this is `C'`)
       C = np.tile(knots,(degree+1,1)); C[0].fill(1)
       np.multiply.accumulate(C[1:], out = C[1:])
 
-
+      # Compute `H` matrix and polynomial coefficients `a`
       H = np.dot(C,C.T)+ L/2.0 * np.identity(degree+1)
       a = np.dot(np.dot(lalg.inv(H),C),points)
 
+      # Eval polynomial in `t`, using fast numpy operations with memory tradeoff
       T = np.tile(t,(degree+1,1)); T[0].fill(1)
       np.multiply.accumulate(T[1:], out = T[1:])
       return np.dot(T.T, a)
 
     else: 
-      C = np.vander(knots, int(degree) + 1)[:,::-1]
+      C = np.vander(knots, int(degree) + 1)[:,::-1] # Slower than our computing
 
       H = np.dot(C.T,C)+ L/2.0 * np.identity(degree+1)
       fit = np.linalg.lstsq(H,np.dot(C.T,points))[0][::-1]
 
       return np.vstack([np.polyval(fit[:,i],t) for i in [0,1]]).T
 
+
+def chebyshev_knots(a, b, n):
+  '''
+  Computes n chebyshev_knots in the interval [a,b]
+
+  returns: numpy.array of the nodes
+  '''
+  return ((a+b) + (b-a) * np.cos( (2*np.arange(1,n+1) - 1)*np.pi / (2.0*n) )) / 2.0
+
+
+
 if __name__ == '__main__':
 
-    n = 5.0 # 19.0 ?!
+    n = 10.0
     tau = np.arange(1,n+1)
     x = np.vstack((tau,np.random.randint(-n, n, size=n))).T
     num_points = 100
 
-    poly_1 = polynomial_curve_fitting(x, tau, 'least_squares', num_points, degree = n-1, L=0,libraries = False)
-    #poly_1 = newton_polynomial(x, tau, num_points, libraries=True)
+    poly_1 = polynomial_curve_fitting(x, tau, 'least_squares', num_points, degree = None, L=0, libraries = False)
     poly_0 = polynomial_curve_fitting(x, tau, 'newton', num_points, libraries=False, L=0, degree = 2)
     print np.linalg.norm(poly_0 - poly_1)
     
@@ -223,10 +171,26 @@ if __name__ == '__main__':
     
     
     import timeit
-    
-    print(timeit.repeat("x = np.random.randint(-10, 10, size=n); newton_polynomial(x, tau, libraries=False)",
-                        setup="from __main__ import newton_polynomial, n,  tau, np",
-                        number=10000))
-    print(timeit.repeat("x = np.random.randint(-10, 10, size=n); newton_polynomial(x, tau, libraries=True)",
-                        setup="from __main__ import newton_polynomial, n,  tau, np",
-                        number=10000))
+
+    setup = '''
+import numpy as np
+from polynomial_curve_fitting import polynomial_curve_fitting
+n=10
+knots = np.linspace(0, 1, n)
+num_points = 200
+    '''
+    print 'newton:', min(timeit.repeat("x = np.random.randint(-10, 10, size=(n, 2));\
+        polynomial_curve_fitting(x, knots, method='newton',\
+        libraries=False, num_points=num_points)", setup=setup, number=10000))
+        
+    print 'newton_libraries:', min(timeit.repeat("x = np.random.randint(-10, 10, size=(n, 2));\
+        polynomial_curve_fitting(x, knots, method='newton',\
+        libraries=False, num_points=num_points)", setup=setup, number=10000))
+        
+    print 'least_squares:', min(timeit.repeat("x = np.random.randint(-10, 10, size=(n, 2));\
+        polynomial_curve_fitting(x, knots, method='least_squares',\
+        libraries=False, num_points=num_points, L=1e-10)", setup=setup, number=10000))
+
+    print 'least_squares_libraries:', min(timeit.repeat("x = np.random.randint(-10, 10, size=(n, 2));\
+        polynomial_curve_fitting(x, knots, method='least_squares',\
+        libraries=True, num_points=num_points, L=1e-10)", setup=setup, number=10000))
