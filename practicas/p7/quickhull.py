60abda53891d2c02217dbd3dd65c29675be3c524
Pablo Cabeza <josepablocg@gmail.com>
2015-05-10 03:30:44 +0200

    quickhull working for flop

--- practicas/p7/quickhull.py
+++ practicas/p7/quickhull.py
@@ -9,48 +9,22 @@ def pplot(points, *args, **kwargs):
     plt.plot(points[:, 0], points[:, 1], *args, **kwargs)
 
 
-def qhull(points):
-    '''
-    Idea sacada de (http://en.literateprograms.org/Quickhull_(Python,_arrays))
-    '''
-    def link(a, b): return np.concatenate((a, b[1:]))
-
-    def edge(a, b): return np.concatenate(([a], [b]))
-
-    def dome(points, base):
-        h, t = base
-        dists = np.dot(points - h, np.dot([(0, -1), (1, 0)], (t - h)))
-        outer = np.repeat(points, dists > 0, axis=0)
-
-        if len(outer):
-            pivot = points[np.argmax(dists)]
-            return link(dome(outer, edge(h, pivot)),
-                        dome(outer, edge(pivot, t)))
-        else:
-            return base
-
-    if len(points) > 2:
-        axis = points[:, 0]
-        base = np.take(points, [np.argmin(axis), np.argmax(axis)], axis=0)
-        return link(dome(points, base),
-                    dome(points, base[::-1]))
-    else:
-        return points
-
-
-def quickhull(points, eps=0.01, segment=None):
+def quickhull(pts, eps=0.01, segment=None):
     '''
     Computes the convex hull of a set of points.
     Inpired in [this quickhull](http://en.literateprograms.org/Quickhull_(Python,_arrays))
 
+    To compute `dists`, we find a normal vector to the segment and then
+    project each point to that vector using the `np.dot` product.
+
     Arguments
     ---------
-    - `points`: `numpy.array(shape=(n,2))` of points to compute the hull
+    - `pts`: `numpy.array(shape=(n,2))` of points to compute the hull
 
     Keyword argumens
     ----------------
     - `eps`: the threadhold to compare with zero
-    - `segment`: a pair (m,M) that divides the set of points. If `None`, then
+    - `segment`: a pair (a,b) that divides the set of points. If `None`, then
       use as segment the extremes x-value points and initialize quickhull
 
     Return
@@ -58,35 +32,50 @@ def quickhull(points, eps=0.01, segment=None):
     The resulting hull in counterclockwise order as a (m,2) shape `numpy.array`
     '''
 
-    # initialization
-    if segment is None:
-        xaxis = points[:, 0]
-        m, M = points[np.argmin(xaxis)], points[np.argmax(xaxis)]
-        return quickhull(points, eps, (m, M)) + quickhull(points, eps, (M, m))[1:]
+    if segment is None:  # initialization, find extremes and call quickhull
+        xaxis = pts[:, 0]
+
+        m = pts.repeat(xaxis == np.amin(xaxis), axis=0)
+        m = m[np.argmin(m[:, 1])]
+        
+        M = pts.repeat(xaxis == np.amax(xaxis), axis=0)
+        M = M[np.argmax(M[:, 1])]
+
+        return quickhull(pts, eps, (m, M)) + quickhull(pts, eps, (M, m))[1:]
+
     sB, sE = segment; vector = sE - sB
+    dists = np.dot(pts - sB, [-vector[1], vector[0]])
+    outer = pts.repeat(dists > eps, axis=0)  # take dists > eps elements
 
-    dists = np.dot(points - sB, [-vector[1], vector[0]])
-    outer = np.repeat(points, dists > eps, axis=0)  # faster than points[dists > 0] ?
-
-    # base case: no points above
-    if not outer.size: return segment
+    if not outer.size: return segment  # base case: no points above
 
     # recursive: find pivot and repeat over new segments
-    pivot = points[np.argmax(dists)]
+    pivot = pts[np.argmax(dists)]
     return quickhull(outer, eps, segment=(sB, pivot)) +\
            quickhull(outer, eps, segment=(pivot, sE))[1:]
 
 
+def convex_hull(pts): return quickhull(np.array(pts), eps=0.0)
+
+
+'''
 if __name__ == "__main__":
 
     npoints = 30
-    points = np.random.randint(-10, 10, size=(npoints, 2))
 
-    hull = np.array(quickhull(points))
+    # points = np.random.randint(-10, 10, size=(npoints, 2))
+    # hull = np.array(quickhull(points))
+
+    # points = np.array([[-1, 0], [1, 0], [1, 1], [-1, 0], [1, 0], [1, 1]])
+    # points = np.array([[0, 0], [0, 1], [0, 2], [0, 3], [0, 1]])
+
+    execfile("data.py")
+    points = np.array(points)
+    hull = np.array(convex_hull(points))
     print(hull)
 
-    plt.plot(points[:, 0], points[:, 1], 'or')
     plt.plot(hull[:, 0], hull[:, 1])
+    plt.plot(points[:, 0], points[:, 1], 'r.')
 
     # import time
     # for i in xrange(len(hull) - 1):
@@ -94,3 +83,4 @@ if __name__ == "__main__":
     #     time.sleep(1)
 
     plt.show()
+'''
