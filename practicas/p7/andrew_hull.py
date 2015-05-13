2e2e66eb7f60b9ba25f3e9fc958b305f3cd144f6
Pablo Cabeza <josepablocg@gmail.com>
2015-05-13 17:46:33 +0200

    cleaned p7, split quickhull for performance

--- practicas/p7/andrew_hull.py
+++ practicas/p7/andrew_hull.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python2
+# -*- encoding: utf-8 -*-
 
 '''
 Computational Geometry Assignments | (c) 2015 Pablo Cabeza & Diego González
 license: modified BSD
 '''
 
-
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def andrews_hull(pts):
@@ -24,7 +23,6 @@ def andrews_hull(pts):
     ------
     A list of point pairs that conforms the convex hull clockwise starting from
     the lowest lexicographic-wise point
-
     '''
     pts = sorted(set(tuple(p) for p in pts))
 
@@ -41,49 +39,4 @@ def andrews_hull(pts):
     return (dome(pts)[:-1] + dome(reversed(pts)))[::-1]
 
 
-# def andrews_hull2(pts):
-#     pts = sorted(set(tuple(p) for p in pts))
-#
-#     def cross(O, A, B): return (A[0] - O[0]) * (B[1] - O[1]) - (A[1] - O[1]) * (B[0] - O[0])
-#
-#     r = []; lwr = r
-#     for p in pts:
-#         while len(r) >= 2 and cross(r[-2], r[-1], p) <= 0: r.pop()
-#         r.append(p)
-#
-#     r = []; upp = r
-#     for p in reversed(pts):
-#         while len(r) >= 2 and cross(r[-2], r[-1], p) <= 0: r.pop()
-#         r.append(p)
-#
-#     return np.array((lwr[:-1] + upp)[::-1])
-#
 convex_hull = andrews_hull
-
-
-'''
-if __name__ == "__main__":
-    # assert convex_hull([(i / 10, i % 10)
-    #                 for i in range(100)]) == [(0, 0), (9, 0), (9, 9), (0, 9)]
-
-    npoints = 30
-
-    # points = np.random.randint(-10, 10, size=(npoints, 2))
-    # hull = np.array(quickhull(points))
-
-    # points = np.array([[0, 0], [1, 0]])
-    # points = np.array([[-1, 0], [1, 1], [1, 0], [-1, 0], [1, 0], [1, 1]])
-    # points = np.array([[0, 0], [0, 1], [0, 2], [0, 3], [0, 1]])
-
-    execfile("data2.py")
-    points = np.array(points)
-
-    hull = (convex_hull(points))
-    print(hull)
-    print(result)
-
-    hull = np.array(hull)
-    plt.plot(hull[:, 0], hull[:, 1])
-    plt.plot(points[:, 0], points[:, 1], 'r.')
-    plt.show()
-'''
