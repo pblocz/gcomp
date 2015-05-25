2e578da8fbc458957f800fcc5672d38e5dce4330
Pablo Cabeza <josepablocg@gmail.com>
2015-05-25 19:24:48 +0200

    cleaned bezier intersection

--- practicas/04_bezier_intersection/interfaz.py
+++ practicas/04_bezier_intersection/interfaz.py
@@ -1,111 +1,24 @@
-# -*- coding: utf-8 -*-
-from Tkinter import * 
+#!/usr/bin/env python2
+# coding=utf-8
+
+'''
+Computational Geometry Assignments | (c) 2015 Pablo Cabeza & Diego González
+license: [modified BSD](http://opensource.org/licenses/BSD-3-Clause)
+'''
+
+
+from Tkinter import *
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 from scipy.special import binom
 from matplotlib.patches import Polygon
-from matplotlib.lines import Line2D 
+from matplotlib.lines import Line2D
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.backend_bases import key_press_handler
 
-
-
-class FastIntersectionBezierCore(object):
-
-    _EPSILON = 0.01
-    
-    @staticmethod
-    def intersect_bbox_check(pA, pB, epsilon = _EPSILON):
-        AX, AY = np.amax(pA,axis=0); Ax, Ay = np.amin(pA, axis=0)
-        BX, BY = np.amax(pB,axis=0); Bx, By =  np.amin(pB, axis=0)
-        return Ax <= BX and AX >= Bx and Ay <= BY and AY >= By
-
-    @staticmethod
-    def division(poly, t = 0.5):
-        divA, divB = np.empty(poly.shape), np.empty(poly.shape)
-        divA[0], divB[0] = poly[0], poly[-1]
-
-        curr = np.copy(poly)
-        for j in xrange(1,len(poly)): 
-            np.add(curr,np.roll(curr,-1,axis=0), curr)
-            curr = np.multiply(curr,t)[:-1]
-            # np.multiply(curr,t,curr) # curr *= t
-
-            divA[j], divB[j] = curr[0], curr[-1]
-
-        return (divA, divB)
-
-    @staticmethod
-    def seg_intersect(a1,a2, b1,b2) :
-        da = a2-a1; da[0], da[1] = da[1], -da[0]
-        db = b2-b1
-        return (np.dot(da,a1-b1)/np.dot(da,db))*db + b1
-        # return db*np.dot(da,a1-b1)/np.dot(da,db) + b1
-
-
-
-class IntersectionBezier(FastIntersectionBezierCore):
-    _EPSILON = FastIntersectionBezierCore._EPSILON
-    
-    @staticmethod
-    def _delta22(poly):
-        a =  np.roll(poly,-1,0)
-        np.subtract(poly,a,a)
-        return np.subtract(a,np.roll(a,-1,0),a)[:-2]
-
-    def __call__(self, polyA, polyB, epsilon = _EPSILON):
-        '''
-        args:
-        - polyA: shape = (n,2)
-        - polyB: shape = (m,2)
-        
-        kwargs:
-        - epsilon: zero threashold
-
-        return: k intersection points numpy array with shape = (k,2)
-        '''
-        self.lastPolyA, self.lastPolyB = polyA, polyB
-
-        if self.intersect_bbox_check(polyA,polyB):
-            deltaA = self._delta22(polyA)
-            deltaB = self._delta22(polyB)
-
-            DA, DB = np.amax(np.absolute(deltaA)), np.amax(np.absolute(deltaB))
-            n, m, eps = len(polyA), len(polyB), epsilon*8
-
-            if n*(n-1)*DA > eps: polyA, (divA,divB) = polyB, self.division(polyA)
-            elif m*(m-1)*DB > eps: polyA, (divA,divB) = polyA, self.division(polyB)
-            else: return [self.seg_intersect(polyA[0], polyA[-1],polyB[0], polyB[-1])] 
-
-            ret = self(polyA,divA, epsilon = epsilon)
-            ret.extend(self(polyA,divB, epsilon = epsilon))
-
-            self.intersectionPoints = ret
-            return ret
-        self.intersectionPoints = []
-        return []
-
-
-    def _plot(self, polyA, k = 3):
-        '''
-        Plots the curve with k steps
-
-        kwargs:
-        - k: the number of subdivision steps
-        - figure: the matplotlib figure to plot
-        '''
-        if len(polyA) == 0: return []
-        if k == 0: 
-            return polyA
-        
-        divA, divB = self.division(polyA)
-
-        r = self._plot(divA, k - 1)
-        return np.concatenate((r,self._plot(divB[::-1], k - 1)))
-
-
+from intersection_bezier import IntersectionBezier
 
 
 class DrawCurves(object):
@@ -121,22 +34,22 @@ class DrawCurves(object):
         self.selected_polygon = True
         self.k = 3
         self.epsilon = 0.01
-        
+
         self.cid_press = self.plt_pointsA.figure.canvas.mpl_connect('button_press_event', self.press_event)
         self.cid_move = self.plt_pointsA.figure.canvas.mpl_connect('motion_notify_event', self.move_event)
         self.cid_release = self.plt_pointsA.figure.canvas.mpl_connect('button_release_event', self.release_event)
         self.cid_erease = self.plt_pointsA.figure.canvas.mpl_connect('button_press_event', self.erease_event)
 
         self.intersector = IntersectionBezier()
-    
+
     def update_data(self, k, epsilon):
         self.k = k
         self.epsilon = epsilon
 
     def update_Bezier(self):
-        pA = self.intersector._plot(np.array(self.polygonA), self.k)
+        pA = self.intersector._plot_points(np.array(self.polygonA), self.k)
         self.plt_curveA.set_data([x for x,_ in pA], [y for _,y in pA])
-        pB = self.intersector._plot(np.array(self.polygonB), self.k)
+        pB = self.intersector._plot_points(np.array(self.polygonB), self.k)
         self.plt_curveB.set_data([x for x,_ in pB], [y for _,y in pB])
 
     def update_curve(self):
@@ -151,7 +64,7 @@ class DrawCurves(object):
         intr = self.intersector( np.array(self.polygonA),np.array(self.polygonB),self.epsilon)
         self.plt_inters.set_data([x for x,_ in intr], [y for _,y in intr])
         self.plt_inters.figure.canvas.draw()
-    
+
     def new_point(self, event):
         if self.selected_polygon:
             self.xsA.append(event.xdata)
@@ -165,21 +78,21 @@ class DrawCurves(object):
 
         self.update_curve()
         self.update_intersection()
-         
+
     def press_event(self, event):
-        if (event.inaxes != self.plt_pointsA.axes and event.inaxes != self.plt_pointsB.axes) or event.button != 1: 
+        if (event.inaxes != self.plt_pointsA.axes and event.inaxes != self.plt_pointsB.axes) or event.button != 1:
             return
         if self.selected_polygon:
             state, indices = self.plt_pointsA.contains(event)
         else:
-            state, indices = self.plt_pointsB.contains(event)                
+            state, indices = self.plt_pointsB.contains(event)
         if not state:
             self.new_point(event)
         else:
             self.selected_point = indices['ind'][0]
             # print self.selected_point
-            
-    def move_event(self, event): 
+
+    def move_event(self, event):
         if (event.inaxes != self.plt_pointsA.axes and event.inaxes != self.plt_pointsB.axes) or self.selected_point is None:
             return
         if self.selected_polygon:
@@ -190,16 +103,16 @@ class DrawCurves(object):
             self.xsB[self.selected_point] = event.xdata
             self.ysB[self.selected_point] = event.ydata
             self.polygonB[self.selected_point] = [event.xdata,event.ydata]
-        self.update_curve()    
-        self.update_intersection() 
-        
-    def release_event(self, event):  
-        if event.inaxes != self.plt_pointsA.axes and event.inaxes != self.plt_pointsB.axes: 
+        self.update_curve()
+        self.update_intersection()
+
+    def release_event(self, event):
+        if event.inaxes != self.plt_pointsA.axes and event.inaxes != self.plt_pointsB.axes:
             return
         self.selected_point = None
-        
+
     def erease_event(self, event):
-        if (event.inaxes != self.plt_pointsA.axes and event.inaxes != self.plt_pointsB.axes) or event.button != 3: 
+        if (event.inaxes != self.plt_pointsA.axes and event.inaxes != self.plt_pointsB.axes) or event.button != 3:
             return
         if self.selected_polygon:
             state, indices = self.plt_pointsA.contains(event)
@@ -217,21 +130,21 @@ class DrawCurves(object):
                 del self.polygonB[index]
             self.update_curve()
             self.update_intersection()
-            
+
 
 def main(args=None):
     args = args or sys.argv
-    
+
     #Se crea la ventana
     root = Tk()
     root.title("Intersección de curvas de Bezier")
-    
+
     #Se crea la zona para los botones de seleccion de poligonos
     label = Label(root, text="Elige un polígono")
     label.pack()
     buttons = Frame(root)
-    buttons.pack() 
-    
+    buttons.pack()
+
     #Se crea la grafica
     fig = Figure(figsize=(5,4), dpi=100)
     ax = fig.add_subplot(111)
@@ -242,7 +155,7 @@ def main(args=None):
     toolbar = NavigationToolbar2TkAgg(canvas, root)
     toolbar.update()
     canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=1)
-    
+
     #Se crea la zona de seleccion de datos
     label = Label(root, text="Elige los datos")
     label.pack()
@@ -260,12 +173,12 @@ def main(args=None):
     dataE = Entry(data, text=0.01, textvar=varE)
     dataE.insert(0, "0.01")
     dataE.pack(side=LEFT, padx=2, pady=2)
-        
+
     #Se generan los poligonos y sus curvas de Bezier
     pointsA, = ax.plot([], [], 'ro')
-    pointsB, = ax.plot([], [], 'bo')   
+    pointsB, = ax.plot([], [], 'bo')
     linebuilder = DrawCurves(pointsA, pointsB)
-       
+
     #Una vez generadas las curvas, se generar las funciones que tendran los botones
     def functionA():
         linebuilder.selected_polygon = True
@@ -277,16 +190,16 @@ def main(args=None):
         linebuilder.update_data(int(varK),float(varE))
         linebuilder.update_curve()
         linebuilder.update_intersection()
-       
-        
-    #Una vez que existen las funciones, se crean los botones y se les asignan las acciones 
+
+
+    #Una vez que existen las funciones, se crean los botones y se les asignan las acciones
     buttonA = Button(buttons, text="Polygon A", command=functionA, fg="red" )
     buttonA.pack(side=LEFT, padx=2, pady=2)
     buttonB = Button(buttons, text="Polygon B", command=functionB, fg="blue" )
     buttonB.pack(side=LEFT, padx=2, pady=2)
     buttonAct = Button(root, text="Actualizar", command=actualizar)
     buttonAct.pack()
-    
+
     root.mainloop()
 
 if __name__ == "__main__": sys.exit(main())
--- practicas/04_bezier_intersection/intersection_bezier.py
+++ practicas/04_bezier_intersection/intersection_bezier.py
@@ -1,11 +1,16 @@
-#! /usr/bin/env python
-# -*- encoding: utf-8 -*-
+#!/usr/bin/env python2
+# coding=utf-8
+
+'''
+Computational Geometry Assignments | (c) 2015 Pablo Cabeza & Diego González
+license: [modified BSD](http://opensource.org/licenses/BSD-3-Clause)
+'''
+
+import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import namedtuple, deque
-
-
 
 
 class FastIntersectionBezierCore(object):
@@ -14,58 +19,46 @@ class FastIntersectionBezierCore(object):
     '''
 
     _EPSILON = 0.01
-    
+
     @staticmethod
-    def intersect_bbox_check(pA, pB, epsilon = _EPSILON):
-        AX, AY = np.amax(pA,axis=0); Ax, Ay = np.amin(pA, axis=0)
-        BX, BY = np.amax(pB,axis=0); Bx, By =  np.amin(pB, axis=0)
+    def intersect_bbox_check(pA, pB, epsilon=_EPSILON):
+        AX, AY = np.amax(pA, axis=0); Ax, Ay = np.amin(pA, axis=0)
+        BX, BY = np.amax(pB, axis=0); Bx, By = np.amin(pB, axis=0)
         return Ax <= BX and AX >= Bx and Ay <= BY and AY >= By
 
     @staticmethod
-    def division(poly, t = 0.5):
+    def division(poly, t=0.5):
         divA, divB = np.empty(poly.shape), np.empty(poly.shape)
         divA[0], divB[0] = poly[0], poly[-1]
 
         curr = np.copy(poly)
-        for j in xrange(1,len(poly)): 
-            np.add(curr,np.roll(curr,-1,axis=0), curr)
-            curr = np.multiply(curr,t)[:-1]
+        for j in xrange(1, len(poly)):
+            np.add(curr, np.roll(curr, -1, axis=0), curr)
+            curr = np.multiply(curr, t)[:-1]
             # np.multiply(curr,t,curr) # curr *= t
 
             divA[j], divB[j] = curr[0], curr[-1]
 
         return (divA, divB)
 
-
     @staticmethod
-    def seg_intersect(a1,a2, b1,b2) :
-        da = a2-a1; da[0], da[1] = da[1], -da[0]
-        db = b2-b1
-        return (np.dot(da,a1-b1)/np.dot(da,db))*db + b1
-        # return db*np.dot(da,a1-b1)/np.dot(da,db) + b1
-
+    def seg_intersect(a1, a2, b1, b2):
+        da = a2 - a1; da[0], da[1] = da[1], -da[0]
+        db = b2 - b1
+        return (np.dot(da, a1 - b1) / np.dot(da, db)) * db + b1
 
     @staticmethod
     def _delta2(poly):
-        a =  np.roll(poly,-1,0)
-        np.subtract(poly,a,a)
-
-        return np.subtract(a,np.roll(a,-1,0),a)[:-2]
-
-
+        a = np.roll(poly, -1, 0)
+        np.subtract(poly, a, a)
+        return np.subtract(a, np.roll(a, -1, 0), a)[:-2]
 
 
 class IntersectionBezier(FastIntersectionBezierCore):
-    '''
-    self.polyA
-    self.polyB
-    
-    '''
 
     _EPSILON = FastIntersectionBezierCore._EPSILON
-    
-    
-    def __call__(self, polyA, polyB, epsilon = _EPSILON, firstCall = True):
+
+    def __call__(self, polyA, polyB, epsilon=_EPSILON, firstCall=True):
         '''
         Computes the intersection of the two bezier curves defined by the
         given polygons. It stores the results in:
@@ -76,7 +69,7 @@ class IntersectionBezier(FastIntersectionBezierCore):
         args:
         - polyA: shape = (n,2)
         - polyB: shape = (m,2)
-        
+
         kwargs:
         - epsilon: zero threashold
 
@@ -86,28 +79,27 @@ class IntersectionBezier(FastIntersectionBezierCore):
         if firstCall: self.lastPolyA, self.lastPolyB = polyA, polyB
 
         ret = []
-        if self.intersect_bbox_check(polyA,polyB):
+        if self.intersect_bbox_check(polyA, polyB):
             deltaA = self._delta2(polyA)
             deltaB = self._delta2(polyB)
 
             DA, DB = np.amax(np.absolute(deltaA)), np.amax(np.absolute(deltaB))
-            n, m, eps = len(polyA), len(polyB), epsilon*8
+            n, m, eps = len(polyA), len(polyB), epsilon * 8
 
-            if n*(n-1)*DA > eps: polyA, (divA,divB) = polyB, self.division(polyA)
-            elif m*(m-1)*DB > eps: polyA, (divA,divB) = polyA, self.division(polyB)
-            else: ret = [self.seg_intersect(polyA[0], polyA[-1],polyB[0], polyB[-1])] 
+            if n * (n - 1) * DA > eps:
+                polyA, (divA, divB) = polyB, self.division(polyA)
+            elif m * (m - 1) * DB > eps:
+                polyA, (divA, divB) = polyA, self.division(polyB)
+            else: ret = [self.seg_intersect(polyA[0], polyA[-1], polyB[0], polyB[-1])]
 
-            if not ret: 
-                ret = self(polyA,divA, epsilon = epsilon, firstCall = False)
-                ret.extend(self(polyA,divB, epsilon = epsilon, firstCall = False))
-
+            if not ret:
+                ret = self(polyA, divA, epsilon=epsilon, firstCall=False)
+                ret.extend(self(polyA, divB, epsilon=epsilon, firstCall=False))
 
         self.lastIntersectionPoints = ret
         return ret
 
-
-
-    def __plot_points(self, polyA, k = 3):
+    def _plot_points(self, polyA, k=3):
         '''
         Compute the points of the bezier curve with k steps
 
@@ -116,15 +108,15 @@ class IntersectionBezier(FastIntersectionBezierCore):
         - figure: the matplotlib figure to plot
         '''
         if len(polyA) == 0: return []
-        if k == 0: 
+        if k == 0:
             return polyA
-        
+
         divA, divB = self.division(polyA)
 
-        r = self._plot(divA, k - 1)
-        return np.concatenate((r,self._plot(divB[::-1], k - 1)))
+        r = self._plot_points(divA, k - 1)
+        return np.concatenate((r, self._plot_points(divB[::-1], k - 1)))
 
-    def _plot(self, polyA, k = 3, **kwargs):
+    def _plot(self, polyA, k=3, **kwargs):
         '''
         Plots the curve with k steps
 
@@ -132,41 +124,37 @@ class IntersectionBezier(FastIntersectionBezierCore):
         - k: the number of subdivision steps
         - figure: the matplotlib figure to plot
         '''
-        if k == 0: 
-            plt.plot(polyA[:,0], polyA[:,1], **kwargs)
+        if k == 0:
+            plt.plot(polyA[:, 0], polyA[:, 1], **kwargs)
             return
-        
+
         divA, divB = self.division(polyA)
 
-        self._plot(divA,  k - 1, **kwargs)
-        self._plot(divB,  k - 1, **kwargs)
+        self._plot(divA, k - 1, **kwargs)
+        self._plot(divB, k - 1, **kwargs)
 
-
-    def plot(self, k = 3):
+    def plot(self, k=3):
         ''' Plots the last intersection curves'''
         self._plot(self.lastPolyB, k, color="b")
+
         self._plot(self.lastPolyA, k, color="g")
 
         intr = self.lastIntersectionPoints
-        plt.plot([x for x,_ in intr], [y for _,y in intr], "ro")
+        plt.plot([x for x, _ in intr], [y for _, y in intr], "ro")
 
-
-
-
-import sys
 
 def main(args=None):
     args = args or sys.argv
-    # Generamos interactivo
 
-    puntosA = [[0,0],[1,2],[3,1],[0,1]]
-    puntosB = [[0,2],[1,0],[2,0],[0,2]]
-
+    puntosA = [[0, 0], [1, 2], [3, 1], [0, 1]]
+    puntosB = [[0, 2], [1, 0], [2, 0], [0, 2]]
 
     intersection = IntersectionBezier()
-    intr = intersection(np.array(puntosA),np.array(puntosB))
+    intr = intersection(np.array(puntosA), np.array(puntosB))
 
-    intersection.plot(k = 4)
+    intersection.plot(k=4)
     plt.show()
+
+    return 0
 
 if __name__ == "__main__": sys.exit(main())
